ffad4ebe18df92f7ed5231d09c8c59ee24fbbca4
Stuart Sears <stuart@sjsears.com>
2011-09-14 17:13:53 +0100

    updated the api-scripts files to enable packaging. You can now use python setup.py bdist_rpm in the api-scripts directory to build an RPM of the scripts, deploying into /usr/bin
    TODO: still some files deployed into /usr/docs, which I can't seem to stop.

--- setup.py
+++ setup.py
@@ -2,16 +2,54 @@
 # -*- coding: utf-8 -*-
 # setup script for packaging the rhnapi module
 import sys
-print "currently nonfunctional."
-sys.exit(0)
+# print "currently nonfunctional."
+# sys.exit(0)
 
 from distutils.core import setup
 setup(name = 'satellite-api-utils',
-      version = '1.0',
+      version = '5.4',
       description = 'RHN Satellite XMLRPC API scripts and utilities',
       author = 'Stuart Sears',
       author_email = 'sjs@redhat.com',
-      packages = [ 'scripts' ],
+      scripts = [
+          "scripts/audit-packages.py",
+          "scripts/channel-errata.py",
+          "scripts/channel-org-access.py",
+          "scripts/clone-activationkey.py",
+          "scripts/clone-channel.py",
+          "scripts/clone-configchannel.py",
+          "scripts/clone-errata.py",
+          "scripts/compare-channel-pkglist.py",
+          "scripts/compare-system-to-channel.py",
+          "scripts/create-channel.py",
+          "scripts/delete-activationkey.py",
+          "scripts/delete-channel.py",
+          "scripts/delete-configchannel.py",
+          "scripts/delete-kickstart.py",
+          "scripts/export-activationkeys.py",
+          "scripts/export-configchannels.py",
+          "scripts/export-kickstarts.py",
+          "scripts/import-activationkeys.py",
+          "scripts/import-configchannels.py",
+          "scripts/import-kickstarts.py",
+          "scripts/list-activationkeys.py",
+          "scripts/list-channels.py",
+          "scripts/list-configfiles.py",
+          "scripts/list-duplicate-systems.py",
+          "scripts/list-errata-for-package.py",
+          "scripts/list-unknown-arch-systems.py",
+          "scripts/upload-config-file.py",
+      ],
+      data_files = [
+        ["docs", [ "templates/apiscript.template",
+                   "templates/channel-errata.conf",
+                   "templates/README.templates",
+                   "templates/rhninfo.template",
+                   "README.scripts",
+                   ]
+        ],
+      ],
       )
+
 
 
